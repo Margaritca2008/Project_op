eeef76751eb52c97c5d1f87160fb8c60bbe15d5f
Margaritca2008 <muza20150109@gmail.com>
2025-10-04 20:42:13 +0300

    добавление проекта

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Threading;
+using Program;
+using System.Runtime.InteropServices;
+namespace Program;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Hello!");
+    }
+}
