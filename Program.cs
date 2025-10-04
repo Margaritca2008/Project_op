18a9842d43dd6623543da1587aae61996c88af53
Margaritca2008 <muza20150109@gmail.com>
2025-10-05 00:29:32 +0300

    сделала вывод доски

--- Program.cs
+++ Program.cs
@@ -6,7 +6,8 @@ using System.Drawing;
 using System.Text;
 using System.Threading.Tasks;
 using System.Linq;
-
+using System.Collections;
+using System.Runtime.CompilerServices;
 namespace Program;
 
 class Program
@@ -38,18 +39,63 @@ class Program
                     {
                         if (y < 3) Cells[y, x] = PieceType.Black;
                         else if (y > 4) Cells[y, x] = PieceType.White;
-                        else Cells[y, x] = PieceType.Black;
+                        else Cells[y, x] = PieceType.None;
                     }
                     else
                     {
-                        Cells[y, x] = PieceType.White;
+                        Cells[y, x] = PieceType.None;
                     }
                 }
             }
         }
+        public void Draw()
+        {
+            Console.WriteLine("  1 2 3 4 5 6 7 8");
+            for (int y = 0; y < 8; y++)
+            {
+                Console.Write((y + 1) + " ");
+                for (int x = 0; x < 8; x++)
+                {
+                    bool isDarkCell = (x + y) % 2 == 1;
+                    Console.BackgroundColor = isDarkCell ? ConsoleColor.DarkMagenta : ConsoleColor.White;
+                    var piece = Cells[y, x];
+                    if (piece == PieceType.None)
+                    {
+                        Console.Write("  ");
+                    }
+                    else
+                    {
+                        switch (piece)
+                        {
+                            case PieceType.Black:
+                                Console.ForegroundColor = ConsoleColor.DarkBlue;
+                                Console.Write("● ");
+                                break;
+                            case PieceType.White:
+                                Console.ForegroundColor = ConsoleColor.White;
+                                Console.Write("● ");
+                                break;
+                            case PieceType.BlackKing:
+                                Console.ForegroundColor = ConsoleColor.DarkBlue;
+                                Console.Write("♚ ");
+                                break;
+                            case PieceType.WhiteKing:
+                                Console.ForegroundColor = ConsoleColor.White;
+                                Console.Write("♔ ");
+                                break;
+                        }
+                    }
+                    Console.ResetColor();
+
+                }
+                Console.WriteLine();
+            }
+        }
     }
-    static void Main()
-    {
-        Console.WriteLine("Hello!");
-    }
+        static void Main()
+        {
+            var board = new Board();
+            board.Draw();
+            Console.WriteLine("Hello!");
+        }
 }
